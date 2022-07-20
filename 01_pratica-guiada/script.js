a1bcbeda335410a416572312dc4c57a357d3f337
falacomomenna <danielmenna@gmail.com>
2022-07-20 19:45:53 -0300

    primeira parte da aula

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,17 @@
 //CODIGO AQUI
+let saldo = 15 + 22.4 //setei o saldo inicial
+console.log(`Saldo inicial: ${saldo}`)
+saldo = saldo - 12.34 //saldo inicial subrtraindo 12.34
+console.log(`Saldo atualizado: ${saldo}`)
+saldo = saldo / 3 // Saldo dividido por 3
+console.log(`Saldo após divisão: ${saldo}`)
+saldo = saldo * 3.42 // Saldo após aplicação
+console.log(`Saldo após aplicação: ${saldo}`)
+saldo = saldo % 3
+console.log(`Valor para o chiclete: ${saldo}`)
+
+//ordem
+const multESoma = 2 + 2 * 2
+console.log(multESoma)
+const somaEMult = (2 + 2) * 2
+console.log(somaEMult)
