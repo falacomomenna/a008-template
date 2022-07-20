000a86305b79966727b0f5a6b118155125a78b4e
falacomomenna <danielmenna@gmail.com>
2022-07-20 20:52:22 -0300

    segunda parte da aula

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,4 +1,4 @@
-//CODIGO AQUI
+/* //CODIGO AQUI
 let saldo = 15 + 22.4 //setei o saldo inicial
 console.log(`Saldo inicial: ${saldo}`)
 saldo = saldo - 12.34 //saldo inicial subrtraindo 12.34
@@ -10,8 +10,36 @@ console.log(`Saldo após aplicação: ${saldo}`)
 saldo = saldo % 3
 console.log(`Valor para o chiclete: ${saldo}`)
 
-//ordem
+//ORDEM
 const multESoma = 2 + 2 * 2
 console.log(multESoma)
 const somaEMult = (2 + 2) * 2
 console.log(somaEMult)
+
+let igual = 5 === 5
+console.log(igual)
+let diferente = 5 !== 5
+console.log(diferente)
+
+//Lógicos
+const num1 = 2
+const num2 = 8
+let resultado = num1 === num2
+console.log(`O primeiro numéro é igual ao segundo? ${resultado}`)
+resultado = num1 <= num2
+console.log(`O primeiro numéro é menor ou igual ao segundo? ${resultado}`)
+resultado = num1 > num2
+console.log(`O primeiro numéro é maior ao segundo? ${resultado}`)
+resultado = num1 < num2
+console.log(`O primeiro numéro é menor ao seundo? ${resultado}`)  */
+
+let a = 'Quero dirigir um Celta 2012'
+let b = 'Sou maior de idade'
+let c = 'consigo comprar o Celta por 22.000 reais'
+let d = 'consigo alugar o Celta'
+let e = 'tenho carteira de motorista'
+
+a
+!b
+b && !e
+b && (c || d)(b && e) && !a
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,18 @@
 // EXERCICIO 01
 const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+let comparacao = minhaIdade > idadeAmiga
+console.log(`Sua idade é maior do que a do seu melhor amigo? ${comparacao}`)
+let diferenca = minhaIdade - idadeAmiga
+console.log(`a nossa diferença de idade é de ${diferenca} anos`)
 
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+!b && d
+b && a && !d
+a || d
+c && !d
